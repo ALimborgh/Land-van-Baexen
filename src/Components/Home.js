ef9e5cd8dd85541f0cc6082f3a95d6c45f4efb42
Adriaen Limborgh <adriaen95@hotmail.com>
2024-08-29 13:08:13 +0200

    feat: Add lazy loading for images in Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Heading, Link, Image, Text, Container } from '@chakra-ui/react';
+import { Box, Heading, Link, Image, Text, Container, Grid } from '@chakra-ui/react';
+import { Link as RouterLink} from 'react-router-dom';
 import headerimg from '../Assets/Images/herenboeren_kopafbeelding.jpg';
 import watimg from '../Assets/Images/Ivar-Pel-10-1024x683-600x300.jpg';
 import hoeimg from '../Assets/Images/Uitleveren-foto-Marc-Bolsius-2-1024x683-600x300.jpg';
@@ -20,42 +21,59 @@ const Home = () => {
             Wij willen grip op ons eten, gezond eten, weten waar het vandaan komt en hoe het geproduceerd wordt.
           </Text>
         </Box>
-      </Container>
-      <Container as="main" maxW="container.xl" py={1}>
-        <Box as="section" mb={8}>
-          <Heading as="h2" size="md">Wat is een Herenboerderij?</Heading>
-          <Image src={watimg} alt="Foto Wat" w="50%" />
-          <Text>
-            Wij willen een goed georganiseerde coöperatieve boerderij opzetten. 
-            Het wordt een plek waar duurzaam, puur en eerlijk voedsel wordt geproduceerd exclusief voor de leden-Herenboeren: 
-            groenten, aardappelen, fruit, eieren, rund-, varkens- en kippenvlees. 
-            Een ervaren boer is straks verantwoordelijk voor het werk op de boerderij. 
-            Alles is van een hoge kwaliteit en de prijs is vergelijkbaar met die van de supermarkt. 
-            Het wordt een plek waar het goed toeven is voor mens en dier.
-          </Text>
-        </Box>
-      </Container>
-      <Container as="main" maxW="container.xl" py={1}>
-        <Box as="section" mb={8}>
-          <Heading as="h2" size="md">Hoe werkt het?</Heading>
-          <Image src={hoeimg} alt="Foto Hoe" w="50%" />
-          <Text>
-            Om in de regio een Herenboerderij op te starten, 
-            zijn minimaal 150 deelnemende huishoudens nodig. 
-            Daarom zijn we een zoektocht gestart naar mensen die met ons zo’n kleinschalige boerderij willen oprichten en exploiteren. 
-            Ben jij enthousiast? Laat het ons weten en vertel anderen over dit initiatief.
-          </Text>
-        </Box>
-      </Container>
-      <Container as="main" maxW="container.xl" py={1}>
-        <Box as="section" mb={8}>
-          <Heading as="h2" size="md">Waar is het?</Heading>
-          <Image src={waarimg} alt="Foto Waar" w="50%" />
-          <Text>
-            Een Herenboerderij is 15 tot 20 hectare groot en ligt bij voorkeur dichtbij de woonplaats van de Herenboeren. 
-            Als blijkt dat er genoeg geïnteresseerden zijn, gaan we samen met anderen op zoek naar geschikte gronden in de buurt van Roermond.
-          </Text>
-        </Box>
+        <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+          <Box as="section" mb={8}>
+            <Heading as="h2" size="md">Wat is een Herenboerderij?</Heading>
+            <Image src={watimg} alt="Foto Wat" w="100%" />
+            <Text>
+              Wij willen een goed georganiseerde coöperatieve boerderij opzetten. 
+              Het wordt een plek waar duurzaam, puur en eerlijk voedsel wordt geproduceerd exclusief voor de leden-Herenboeren: 
+              groenten, aardappelen, fruit, eieren, rund-, varkens- en kippenvlees. 
+              Een ervaren boer is straks verantwoordelijk voor het werk op de boerderij. 
+              Alles is van een hoge kwaliteit en de prijs is vergelijkbaar met die van de supermarkt. 
+              Het wordt een plek waar het goed toeven is voor mens en dier.
+            </Text>
+            <Link
+              as={RouterLink}
+              to="/about"
+              fontWeight="bold"
+            >
+              Lees hier meer
+            </Link>
+          </Box>
+          <Box as="section" mb={8}>
+            <Heading as="h2" size="md">Hoe werkt het?</Heading>
+            <Image src={hoeimg} alt="Foto Hoe" w="100%" />
+            <Text>
+              Om in de regio een Herenboerderij op te starten, 
+              zijn minimaal 150 deelnemende huishoudens nodig. 
+              Daarom zijn we een zoektocht gestart naar mensen die met ons zo’n kleinschalige boerderij willen oprichten en exploiteren. 
+              Ben jij enthousiast? Laat het ons weten en vertel anderen over dit initiatief.
+            </Text>
+            <Link
+              as={RouterLink}
+              to="/contact"
+              fontWeight="bold"
+            >
+              Lees hier meer
+            </Link>
+          </Box>
+          <Box as="section" mb={8}>
+            <Heading as="h2" size="md">Waar is het?</Heading>
+            <Image src={waarimg} alt="Foto Waar" w="100%" />
+            <Text>
+              Een Herenboerderij is 15 tot 20 hectare groot en ligt bij voorkeur dichtbij de woonplaats van de Herenboeren. 
+              Als blijkt dat er genoeg geïnteresseerden zijn, gaan we samen met anderen op zoek naar geschikte gronden in de buurt van Roermond.
+            </Text>
+            <Link
+              as={RouterLink}
+              to="/contact"
+              fontWeight="bold"
+            >
+              Lees hier meer
+            </Link>
+          </Box>
+        </Grid>
       </Container>
     </Box>
   );
