581476cc72cee6678669c357209e1a82af0b7e3b
Adriaen Limborgh <adriaen95@hotmail.com>
2024-08-29 12:56:51 +0200

    feat: Add social media links to Footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-import { Box, Heading, Text, Link, VStack, HStack} from '@chakra-ui/react';
+import { Box, Heading, Text, Link, VStack, HStack, Image, Grid} from '@chakra-ui/react';
 import FooterYear from './FooterYear';
+import facebook from '../Assets/Images/Logo FB.jpg';
+import instagram from '../Assets/Images/1685374745-5301-Instagram-Logo-scaled.jpg';
 
 const Footer = () => {
   // Function to scroll to the top of the page
@@ -15,7 +17,7 @@ const Footer = () => {
   return (
     <Box as="footer" bg="green.300" color="black" py={10} px={5}>
       <VStack spacing={10} align="stretch" maxW="1200px" mx="auto">
-        <HStack justify="space-between" align="start" flexWrap="wrap" spacing={8}>
+        <Grid templateColumns={{ base: '1fr', md: 'repeat(3, 1fr)' }} gap={80}>
           <Box>
             <Heading as="h3" size="md" mb={4}>Over ons</Heading>
             <Text mb={4}></Text>
@@ -38,7 +40,18 @@ const Footer = () => {
               <Link as={RouterLink} to="Contact" onClick={scrollToTop}>Contact</Link>
             </VStack>
           </Box>
-        </HStack>
+          <Box>
+            <Heading as="h3" size="md" mb={4}>Volg Ons</Heading>
+            <HStack spacing={4}>
+              <Link href="" target="_blank" rel="noopener noreferrer">
+                <Image src={facebook} alt="Facebook" boxSize="24px" _hover={{ opacity: 0.8 }} />
+              </Link>
+              <Link href="" target="_blank" rel="noopener noreferrer">
+                <Image src={instagram} alt="Instagram" boxSize="24px" _hover={{ opacity: 0.8 }} />
+              </Link>
+            </HStack>
+          </Box>
+        </Grid>
         <Box textAlign="center" borderTop="1px" borderColor="gray.700" pt={4}>
           <FooterYear />
           <Text mt={2}>Alle rechten voorbehouden.</Text>
