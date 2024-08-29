32bc70e37a27f16c12afa94ebd923f976fa89949
Adriaen Limborgh <adriaen95@hotmail.com>
2024-08-29 12:46:48 +0200

    feat: Add logo to Header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,17 +7,23 @@ import {
   useDisclosure,
   Stack,
   Link,
+  Image
 } from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { Link as RouterLink } from 'react-router-dom';
+import logo from '../Assets/Images/logo_herenboeren_roermond_wit.png';
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-
   return (
-    <Box  px={4} bg="green.300" boxShadow="md" position="sticky" top="0" zIndex="1000">
+    <Box px={4} bg="green.300" boxShadow="md" position="sticky" top="0" zIndex="1000">
       <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+        <Flex alignItems={'center'}>
+          <Link as={RouterLink} to="/">
+            <Image src={logo} alt="Land van Baexem" />
+          </Link>
+        </Flex>
         <IconButton
           size={'md'}
           icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
@@ -29,50 +35,24 @@ const Header = () => {
           as={'nav'}
           spacing={4}
           display={{ base: 'none', md: 'flex' }}
-          ml="auto"
         >
-          <Link as={RouterLink} to="/" >
-            Home
-          </Link>
-          <Link as={RouterLink} to="/about" >
-            About
-          </Link>
-          <Link as={RouterLink} to="/news" >
-            News
-          </Link>
-          <Link as={RouterLink} to="/what" >
-            What
-          </Link>
-          <Link as={RouterLink} to="/Q&A" >
-            Q&A
-          </Link>
-          <Link as={RouterLink} to="/contact" >
-            Contact
-          </Link>
+          <Link as={RouterLink} to="/">Home</Link>
+          <Link as={RouterLink} to="/what">What</Link>
+          <Link as={RouterLink} to="/about">About</Link>
+          <Link as={RouterLink} to="/Q&A">Q&A</Link>
+          <Link as={RouterLink} to="/news">News</Link>
+          <Link as={RouterLink} to="/contact">Contact</Link>
         </HStack>
       </Flex>
-
       {isOpen ? (
         <Box pb={4} display={{ md: 'none' }}>
           <Stack as={'nav'} spacing={4}>
-            <Link as={RouterLink} to="/" onClick={onClose} >
-              Home
-            </Link>
-            <Link as={RouterLink} to="/about" onClick={onClose} >
-              About
-            </Link>
-            <Link as={RouterLink} to="/news" onClick={onClose} >
-              News
-            </Link>
-            <Link as={RouterLink} to="/what" onClick={onClose} >
-              What
-            </Link>
-            <Link as={RouterLink} to="/Q&A" onClick={onClose} >
-              Q&A
-            </Link>
-            <Link as={RouterLink} to="/contact" onClick={onClose} >
-              Contact
-            </Link>
+            <Link as={RouterLink} to="/">Home</Link>
+            <Link as={RouterLink} to="/what">What</Link>
+            <Link as={RouterLink} to="/about">About</Link>
+            <Link as={RouterLink} to="/Q&A">Q&A</Link>
+            <Link as={RouterLink} to="/news">News</Link>
+            <Link as={RouterLink} to="/contact">Contact</Link>
           </Stack>
         </Box>
       ) : null}
