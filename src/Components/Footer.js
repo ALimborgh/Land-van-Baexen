74c1ee5f5295f1aeccc7c69755575fa1ca5a9824
Adriaen Limborgh <adriaen95@hotmail.com>
2024-10-01 17:11:12 +0200

    Refactor Footer component and update social media links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box, Heading, Text, VStack, HStack, Image, Grid, Link } from '@chakra-ui/react';
 import FooterYear from './FooterYear';
 import facebook from '../Assets/Images/Logo FB.jpg';
-import instagram from '../Assets/Images/1685374745-5301-Instagram-Logo-scaled.jpg';
+import LvO from '../Assets/Images/LvO-logo-donkergroen-RGB-handtekening.png';
 
 const Footer = () => {
   return (
@@ -24,11 +24,11 @@ const Footer = () => {
           <Box>
             <Heading as="h3" size="md" mb={4}>Volg Ons</Heading>
             <HStack spacing={4}>
-              <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                <Image src={facebook} alt="Facebook" boxSize="24px" _hover={{ opacity: 0.8 }} />
+              <Link href="https://www.facebook.com/LandVanBaexen" target="_blank" rel="noopener noreferrer">
+                <Image src={facebook} alt="" boxSize="24px" _hover={{ opacity: 0.8 }} />
               </Link>
-              <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                <Image src={instagram} alt="Instagram" boxSize="24px" _hover={{ opacity: 0.8 }} />
+              <Link href="https://landvanons.nl/" target="_blank" rel="noopener noreferrer">
+                <Image src={LvO} alt="" boxSize="24px" _hover={{ opacity: 0.8 }} />
               </Link>
             </HStack>
           </Box>
