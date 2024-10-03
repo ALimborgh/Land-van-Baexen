e1d5fa7c716eef07ca622ff605558cca97ceecf4
Adriaen Limborgh <adriaen95@hotmail.com>
2024-10-03 11:50:34 +0200

    Refactor Nieuwsbrieven.js to add latest newsletter

--- src/Assets/Nieuwsbrieven/Nieuwsbrieven.js
+++ src/Assets/Nieuwsbrieven/Nieuwsbrieven.js
@@ -6,6 +6,13 @@ const nieuwsbrieven =[
         description: "Geslaagde Oprichtingsvergadering. Appelplukdagen: 1e op 28 september (plus routebeschrijving)",
         link: "https://landvanbaexen.email-provider.eu/web/p5zkub7rma/iosarec2sf/d1mmbrmf3o/fmi8cnl0j1"
     },
+    {
+        id: 2,
+        title: "Nieuwsbrief Augustus 2024",
+        date: "08-22-2024",
+        description: "Oprichtingsvergadering 21 september 2024",
+        link: "https://landvanbaexen.email-provider.eu/web/p5zkub7rma/iosarec2sf/d1mmbrmf3o/fmi8cnl0j1"
+    },
 ]
 
 export default nieuwsbrieven;
