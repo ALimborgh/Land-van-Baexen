1bf9856fe35686ce80416b4868788bf6689f93c0
Adriaen Limborgh <adriaen95@hotmail.com>
2024-10-03 11:42:57 +0200

    Refactor News component to display sorted newsletters

---
+++ src/Assets/Nieuwsbrieven/Nieuwsbrieven.js
@@ -0,0 +1,11 @@
+const nieuwsbrieven =[
+    {
+        id: 1,
+        title: "Nieuwsbrief September 2024",
+        date: "09-22-2024",
+        description: "Geslaagde Oprichtingsvergadering. Appelplukdagen: 1e op 28 september (plus routebeschrijving)",
+        link: "https://landvanbaexen.email-provider.eu/web/p5zkub7rma/iosarec2sf/d1mmbrmf3o/fmi8cnl0j1"
+    },
+]
+
+export default nieuwsbrieven;
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { Box, Container, Heading, Text, Image } from '@chakra-ui/react';
+import { Box, Container, Heading, Text, Image, Link, VStack } from '@chakra-ui/react';
 import headerimg from '../Assets/Images/herenboeren_kopafbeelding.jpg';
+import nieuwsbrieven from '../Assets/Nieuwsbrieven/Nieuwsbrieven';
 
 const News = () => {
+  // Sort newsletters by date in descending order
+  const sortedNewsletters = nieuwsbrieven.sort((a, b) => new Date(b.date) - new Date(a.date));
+
   return (
     <Box>
       <Image src={headerimg} alt="Land van Beaxen Header" w="100%" />
       <Container as="main" maxW="container.xl" py={8}>
         <Box as="section" mb={8}>
           <Heading as="h2" size="md">Nieuws</Heading>
-          <Text>Hier komt dan het recente nieuws te staan.</Text>
+          <Text>Hieronder een overzicht van de nieuwsbrieven.</Text>
         </Box>
+        <VStack spacing={4} align="stretch">
+          {sortedNewsletters.map((newsletter, index) => (
+            <Box key={index} p={4} borderWidth="1px" borderRadius="md">
+              <Heading as="h3" size="sm">{newsletter.title}</Heading>
+              <Text>{new Date(newsletter.date).toLocaleDateString()}</Text>
+              <Text>{newsletter.description}</Text>
+              <Link href={newsletter.link} color="blue.500" isExternal>
+                Lees meer
+              </Link>
+            </Box>
+          ))}
+        </VStack>
       </Container>
     </Box>
   );
