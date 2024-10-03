bfd57b84ce55e15567336759ee9ec5b7ddeb07aa
Adriaen Limborgh <adriaen95@hotmail.com>
2024-10-03 12:00:59 +0200

    Refactor Header component to update navigation links and add responsive menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,49 +12,47 @@ import {
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { Link as RouterLink } from 'react-router-dom';
 import logo from '../Assets/Images/Logo_opbeige.png';
-/*import logo2 from '../Assets/Images/Logo_optransparant.png';*/
-/*import logo3 from '../Assets/Images/Logo_zwart-wit.png';*/
 
 const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
     <Box px={4} bg="#a1c33e" boxShadow="md" position="sticky" top="0" zIndex="1000">
-      <Flex h={16} alignItems={'center'} justifyContent={'center'} position="relative">
+      <Flex h={16} alignItems="center" justifyContent="center" position="relative">
         <Link as={RouterLink} to="/" position="absolute" left="0%">
           <Box>
             <Image src={logo} alt="Land van Baexem" objectFit="cover" width="100px" />
           </Box>
         </Link>
         <IconButton
-          size={'md'}
+          size="md"
           icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-          aria-label={'Open Menu'}
+          aria-label={isOpen ? 'Close Menu' : 'Open Menu'}
           display={{ md: 'none' }}
           onClick={isOpen ? onClose : onOpen}
           position="absolute"
           right={4}
         />
         <HStack
-          as={'nav'}
+          as="nav"
           spacing={4}
           display={{ base: 'none', md: 'flex' }}
           position="absolute"
           right={4}
         >
-          <Link as={RouterLink} to="/">Home</Link>
-          <Link as={RouterLink} to="/about">Over ons</Link>
-          <Link as={RouterLink} to="/news">Nieuws</Link>
-          <Link as={RouterLink} to="/contact">Contact</Link>
+          <Link as={RouterLink} to="/" _hover={{ textDecoration: 'underline' }}>Home</Link>
+          <Link as={RouterLink} to="/about" _hover={{ textDecoration: 'underline' }}>Over ons</Link>
+          <Link as={RouterLink} to="/news" _hover={{ textDecoration: 'underline' }}>Nieuws</Link>
+          <Link as={RouterLink} to="/contact" _hover={{ textDecoration: 'underline' }}>Contact</Link>
         </HStack>
       </Flex>
       {isOpen ? (
         <Box pb={4} display={{ md: 'none' }}>
-          <Stack as={'nav'} spacing={4}>
-            <Link as={RouterLink} to="/">Home</Link>
-            <Link as={RouterLink} to="/about">Over ons</Link>
-            <Link as={RouterLink} to="/news">Nieuws</Link>
-            <Link as={RouterLink} to="/contact">Contact</Link>
+          <Stack as="nav" spacing={4} alignItems="center">
+            <Link as={RouterLink} to="/" onClick={onClose}>Home</Link>
+            <Link as={RouterLink} to="/about" onClick={onClose}>Over ons</Link>
+            <Link as={RouterLink} to="/news" onClick={onClose}>Nieuws</Link>
+            <Link as={RouterLink} to="/contact" onClick={onClose}>Contact</Link>
           </Stack>
         </Box>
       ) : null}
